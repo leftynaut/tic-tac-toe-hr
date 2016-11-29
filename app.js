898c72f4d8c2aac497f947d84ff89752fc21bf07
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2016-11-29 15:29:59 -0600

    Play again logic broken

--- app.js
+++ app.js
@@ -3,6 +3,19 @@ let board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
 
 let gameFinished = false
 let moveCount = 1
+let playAgain = false
+
+const getCurrentNameAndPiece = function getCurrentNameAndPiece(){
+  let name, char
+  if (moveCount % 2 === 0) {
+    name = playerTwo
+    char = 'O'
+  } else {
+    name = playerOne
+    char = 'X'
+  }
+  return [name, char]
+}
 
 const insertMove = function insertMove(move, char) {
   if (board[move - 1] === 'X' || board[move - 1] === 'O'){
@@ -17,7 +30,14 @@ const insertMove = function insertMove(move, char) {
   }
   board[move - 1] = char
   moveCount++
+  checkForWinner()
   printBoard()
+}
+
+const askForMove = function askForMove() {
+  let [name, char] = getCurrentNameAndPiece()
+  let move = readlineSync.question(`\n${name}, where would you like to place an ${char}? `)
+  insertMove(move, char)
 }
 
 const printBoard = function printBoard() {
@@ -26,19 +46,11 @@ console.log(`\n ${board[0]} | ${board[1]} | ${board[2]}
  ${board[3]} | ${board[4]} | ${board[5]}
 -----------
  ${board[6]} | ${board[7]} | ${board[8]}`)
- checkForWinner()
 }
 
 const checkForWinner = function checkForWinner(){
-  let name
-  if (moveCount % 2 === 0) {
-    name = playerTwo
-    char = 'O'
-  } else {
-    name = playerOne
-    char = 'X'
-  }
-  const checkInnerWinner = function checkInnerWinner(char, name){
+  let [name, char] = getCurrentNameAndPiece()
+  const checkInnerWinner = function checkInnerWinner(name, char){
     if (board[0] === char && board[1] === char && board[2] === char ||
       board[3] === char && board[4] === char && board[5] === char ||
       board[6] === char && board[7] === char && board[8] === char ||
@@ -48,27 +60,15 @@ const checkForWinner = function checkForWinner(){
       board[0] === char && board[4] === char && board[8] === char ||
       board[2] === char && board[4] === char && board[6] === char) {
         gameFinished = true
-        console.log(`\nLooks like ${char} is the winner! Play again?`)
+        playAgain = readlineSync.keyInYN(`\nLooks like ${name} is the winner! Play again?`)
+        playAgain === 'y' ? playAgain = true : playAgain = false
       }
   }
-  checkInnerWinner(char, name)
+  checkInnerWinner(name, char)
   if (moveCount === 9){
     gameFinished = true
     console.log(`\nLooks like it's a cat's game. Play again?`)
   }
-}
-
-const askForMove = function askForMove() {
-  let name, char, move
-  if (moveCount % 2 === 0) {
-    name = playerTwo
-    char = 'O'
-  } else {
-    name = playerOne
-    char = 'X'
-  }
-  move = readlineSync.question(`\n${name}, where would you like to place an ${char}? `)
-  insertMove(move, char)
 }
 
 const playerOne = readlineSync.question('What is your name, Player 1? ');
@@ -78,4 +78,13 @@ printBoard()
 console.log('\nEach turn, you will pick a number that corresponds to your move')
 while (!gameFinished){
   askForMove()
+}
+
+if (playAgain){
+  gameFinished = false;
+  board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+  moveCount = 1
+  while (!gameFinished){
+    askForMove()
+  }
 }
