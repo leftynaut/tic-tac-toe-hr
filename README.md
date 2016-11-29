30fee6468983648ce1e28f5ac0cbe427e76cf42d
Michael Lefkowitz <lefkowitz.michael@gmail.com>
2016-11-29 15:03:57 -0600

    Modular function

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # tic-tac-toe-hr
-A CLI Tic Tac Toe Game
+A CLI Tic Tac Toe Game built in < 1'
+
+Run `node app.js` to begin your game.
--- app.js
+++ app.js
@@ -1,14 +1,41 @@
 const readlineSync = require('readline-sync')
-let board = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+let board = [1, 2, 3, 4, 5, 6, 7, 8, 9]
+
+let gameFinished = false
+
+const insertMove = function insertMove(move, char) {
+  board[move - 1] = char
+  printBoard()
+}
+
+const printBoard = function printBoard() {
+console.log(`\n ${board[0]} | ${board[1]} | ${board[2]}
+-----------
+ ${board[3]} | ${board[4]} | ${board[5]}
+-----------
+ ${board[6]} | ${board[7]} | ${board[8]}`)
+}
+
+let moveCount = 1
+const askForMove = function askForMove() {
+  let name, char, move
+  if (moveCount % 2 === 0) {
+    name = playerTwo
+    char = 'O'
+  } else {
+    name = playerOne
+    char = 'X'
+  }
+  move = readlineSync.question(`\n${name}, where would you like to place an ${char}? `)
+  moveCount++
+  insertMove(move, char)
+}
 
 const playerOne = readlineSync.question('What is your name, Player 1? ');
 const playerTwo = readlineSync.question('And what is your name, Player 2? ');
 console.log('The board is shown below:')
-console.log(`\n ${board[0][0]} | ${board[0][1]} | ${board[0][2]}
------------
- ${board[1][0]} | ${board[1][1]} | ${board[1][2]}
------------
- ${board[2][0]} | ${board[2][1]} | ${board[2][2]}`)
+printBoard()
 console.log('\nEach turn, you will pick a number that corresponds to your move')
-let move;
-move = readlineSync.question(`\n${playerOne}, where would you like to place an X? `)
+while (!gameFinished){
+  askForMove()
+}
